b9f055fb47496163584fef5fceb8b16d443b1687
Zach Puller <zach.puller@gmail.com>
2024-08-29 17:32:54 -0700

    usage / readme

--- gray_pdf.sh
+++ gray_pdf.sh
@@ -1,8 +1,9 @@
+INPUT_PATH=$1
 rm -rf tmp/ outputs/
 mkdir tmp/ outputs/
-python3 py/split_pdf.py inputs/etudes.pdf tmp
+python3 py/split_pdf.py $INPUT_PATH tmp
 ls tmp | xargs -I{} qlmanage -t -s 1600 -o tmp tmp/{} 2>&1 > /dev/null
 ls tmp | grep png | xargs -I{} python3 py/gray.py tmp/{} tmp/gray_{}
 ls tmp | grep gray | grep png$ | xargs -I{} sips -s format pdf tmp/{} --out tmp/{}.pdf 2>&1 > /dev/null
-ls tmp | grep pdf.png.pdf | xargs -I{} echo tmp/{} | xargs python3 py/cat_pdfs.py outputs/etudes.pdf
+ls tmp | grep pdf.png.pdf | xargs -I{} echo tmp/{} | xargs python3 py/cat_pdfs.py outputs/$(basename $INPUT_PATH)
 rm -rf tmp/
